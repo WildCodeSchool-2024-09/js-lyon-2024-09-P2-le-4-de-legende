a385ce0a29eac00325b8cdbeeda68632e7da4f80
User-Alnz <alexandre.anavoizard@gmail.com>
2024-11-08 16:42:44 +0100

    save_1-SeasonTab Components | add function displayQualifacation() +CSS styling

--- client/src/components/SeasonTab.tsx
+++ client/src/components/SeasonTab.tsx
@@ -1,11 +1,8 @@
-import "./SeasonTab.css" 
+import "./SeasonTab.css";
 import { useEffect, useState } from "react";
 
-
-//https://v3.football.api-sports.io/fixtures?season=2023&league=39
-
-interface FullJsonFromAPI{
-
+interface FullJsonFromAPI
+{
     result:{
         seasons:{
             groups:{
@@ -13,11 +10,10 @@ interface FullJsonFromAPI{
             }[];
         }[];
     }[];
-
 }
 
-interface table_Data {
-
+interface table_Data
+{
     draw : number;
     goals_conceded : number;
     goals_scored : number;
@@ -35,12 +31,12 @@ interface table_Data {
 
 function   SeasonTab()
 {
-    /*const   ApiKey = import.meta.env.VITE_API_KEY;
-    const   [SeasonTab, setSeasonTab] = useState<table_Data>([]);
+    const   ApiKey = import.meta.env.VITE_API_KEY;
+    const   [SeasonTab, setSeasonTab] = useState<table_Data[]>([]);
 
     useEffect(() => {
 
-        fetch( "https://soccer-football-info.p.rapidapi.com/championships/view/?i=d621ea12d91a8473&l=en_US", 
+        fetch("https://soccer-football-info.p.rapidapi.com/championships/view/?i=d621ea12d91a8473&l=en_US", 
         { 
         headers: {
             'x-rapidapi-key': `${ApiKey}`,
@@ -50,15 +46,25 @@ function   SeasonTab()
 
             if(!response.ok)
                 throw new Error('Error fetching data from API');
-        
+
             return(response.json());
         })
-        .then((data) => {
+        .then((data : FullJsonFromAPI) => {
             setSeasonTab(data.result[0].seasons[4].groups[0].table);
             console.log(SeasonTab);
         })
     
-    }, []); */
+    }, []); 
+
+    function    displayQualifacation(index :number) 
+    {
+        if(index === 0)
+            return (<span className="dislayQualiciation">Champions League</span>);
+                else if(index === 4)
+                    return (<span className="dislayQualiciation">Conference League Qualification</span>);
+                        else if(index === 17)
+                            return(<span className="dislayQualiciation">Relegation</span>);
+    }
 
     return (
         <section className="WrappingTab">
@@ -78,26 +84,32 @@ function   SeasonTab()
              
             <div className="wrapOptions">
 
-                <span className="menuCard1">Team</span>
-                <span className="menuCard2">position</span>
-                <span className="menuCard3">win</span>
-                <span className="menuCard4">draw</span>
-                <span className="menuCard5">loss</span>
+                <span className="menuCard">Position</span>
+                <span className="menuCard2">Team</span>
+                <span className="menuCard">W</span>
+                <span className="menuCard">D</span>
+                <span className="menuCard">L</span>
+                <span className="menuCard">Pts</span>
 
             </div>
 
 
-            {[...SeasonTab].map((eachTeam , index)=> (
+            {SeasonTab.map((eachTeam, index)=> (
 
-                <div key={index} className="wrapContent">
+                <div key={index}>
+                    {displayQualifacation(index)}
+                    < div className="wrapContent">
 
-                    <span className="menuContent1">{eachTeam.team.name}</span>
-                    <span className="menuContent2">{index}</span>
-                    <span className="menuContent3">{eachTeam.win}</span>
-                    <span className="menuContent4">{eachTeam.draw}</span>
-                    <span className="menuContent5">{eachTeam.loss}</span>
+                        <span className="menuContent">{eachTeam.position}</span>
+                        <span className="menuContent2">{eachTeam.team.name}</span>
+                        <span className="menuContent">{eachTeam.win}</span>
+                        <span className="menuContent">{eachTeam.draw}</span>
+                        <span className="menuContent">{eachTeam.loss}</span>
+                        <span className="menuContent">{eachTeam.points}</span>
+                    </div>
 
                 </div>
+
             ))}
             
 
