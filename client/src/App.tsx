698d0165e5db47ff1325aa3fa7298a6dbcf98c32
theo <boronattheo83@gmail.com>
2024-11-13 10:42:21 +0100

    first commit S1US05

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import League from "./components/League";
 
 export default function App() {
   return (
     <div className="document">
       <NavBar />
+      <League />
       <Footer />
     </div>
   );
